fc90c1a5d69790e2ef4b52c38c21b788e9e8fee2
rolandbaer <rbaer@gmx.ch>
2020-12-21 20:23:58 +0100

    prepared for translations

--- simple-matomo-tracking-code.php
+++ simple-matomo-tracking-code.php
@@ -28,7 +28,13 @@ if ( ! class_exists( 'SMTC_Admin' ) ) {
 		static function add_config_page() {
 			global $wpdb;
 			if ( function_exists('add_options_page') ) {
-				add_options_page('Simple Matomo Tracking Code Configuration', 'Simple Matomo Tracking Code', 'manage_options', basename(__FILE__), array('SMTC_Admin','config_page'));
+				add_options_page(
+					__('Simple Matomo Tracking Code Configuration', 'simple-matomo-tracking-code'),
+					__('Simple Matomo Tracking Code', 'simple-matomo-tracking-code'),
+					'manage_options',
+					basename(__FILE__),
+					array('SMTC_Admin','config_page')
+				);
 			}
 		}
 
@@ -54,7 +60,7 @@ if ( ! class_exists( 'SMTC_Admin' ) ) {
 			}
 
 			if ( isset($_POST['submit']) ) {
-				if (!current_user_can('manage_options')) die(__('You cannot edit the Simple Matomo Tracking Code options.'));
+				if (!current_user_can('manage_options')) die(__('You cannot edit the Simple Matomo Tracking Code options.', 'simple-matomo-tracking-code'));
 				check_admin_referer('analyticspp-config');
 				$options['siteid'] = $_POST['siteid'];
 
@@ -96,38 +102,38 @@ if ( ! class_exists( 'SMTC_Admin' ) ) {
 				</script>
 				<h2>Simple Matomo Tracking Code Configuration</h2>
 				<form action="" method="post" id="analytics-conf">
-					<table class="form-table" style="width:100%;">
 					<?php
 					if ( function_exists('wp_nonce_field') )
 						wp_nonce_field('analyticspp-config');
 					?>
-					<p>Matomo, formerly known as Piwik, is a downloadable web analytics software platform
-						free of charge under the GPL license.<br />
-						If you don't have Matomo installed, you can get it at
-						<a href="https://matomo.org/">matomo.org</a>.</p>
 
+					<p>
+						<?php _e("Matomo, formerly known as Piwik, is a downloadable web analytics software platform free of charge under the GPL license.", 'simple-matomo-tracking-code'); ?>
+						<br />
+						<?php _e('If you don\'t have Matomo installed, you can get it at <a href="https://matomo.org/">matomo.org</a>.', 'simple-matomo-tracking-code'); ?>
+					</p>
+
+					<table class="form-table" style="width:100%;">
 					<tr>
 						<th scope="row" valign="top">
-							<label for="siteid">Matomo site id</label>
+							<label for="siteid"><?php _e('Matomo site id', 'simple-matomo-tracking-code'); ?></label>
 						</th>
 						<td>
 							<input id="siteid" name="siteid" class="small-text" type="number" size="3" maxlength="4" value="<?php echo $options['siteid']; ?>" /><br/>
 							<div id="expl">
-								<p>In the Matomo interface, when you "Add Website"
-									you are shown a piece of JavaScript that
-									you are told to insert into the page, in that script is a 
-									unique string that identifies the website you 
-									just defined, that is your site ID (usually "1").
-								<p>Once you have entered your site id in
-									the box above your pages will be trackable by
-									Matomo Web Analytics.</p>
+								<p>
+								<?php _e('In the Matomo interface, when you "Add Website" you are shown a piece of JavaScript that you are told to insert into the page, in that script is a unique string that identifies the website you just defined, that is your site ID (usually "1").', 'simple-matomo-tracking-code'); ?>
+								</p>
+								<p>
+								<?php _e('Once you have entered your site id in the box above your pages will be trackable by Matomo Web Analytics.', 'simple-matomo-tracking-code'); ?>
+								</p>
 							</div>
 						</td>
 					</tr>
 					<tr>
 						<th scope="row" valign="top">
-							<label for="dltracking">Track downloads</label><br/>
-							<small>(default is YES)</small>
+							<label for="dltracking"><?php _e('Track downloads', 'simple-matomo-tracking-code'); ?></label><br/>
+							<small><?php _e('(default is YES)', 'simple-matomo-tracking-code'); ?></small>
 						</th>
 						<td>
 							<input type="checkbox" id="dltracking" name="dltracking" <?php if ($options['dltracking']) echo ' checked="unchecked" '; ?>/> 
@@ -135,40 +141,59 @@ if ( ! class_exists( 'SMTC_Admin' ) ) {
 					</tr>
 					<tr>
 						<th scope="row" valign="top">
-							<label for="matomo_host">Hostname of the matomo server (optional)</label>
+							<label for="matomo_host"><?php _e('Hostname of the matomo server (optional)', 'simple-matomo-tracking-code'); ?></label>
 						</th>
 						<td>
 							<input id="matomo_host" name="matomo_host" type="text" size="40" maxlength="99" value="<?php echo $options['matomo_host']; ?>" /><br/>
 							<div id="expl3">
-								<p>Example: www.yourdomain.com -- Leave blank (default) if this is the same as your website.
-								Do NOT include the http(s):// bit.</p>
+								<p>
+								<?php _e('Example: www.yourdomain.com -- Leave blank (default) if this is the same as your website. Do NOT include the http(s):// bit.', 'simple-matomo-tracking-code'); ?>
+								</p>
 							</div>
 						</td>
 					</tr>
 					<tr>
 						<th scope="row" valign="top">
-							<label for="matomo_baseurl">Base URL path of matomo installation</label>
+							<label for="matomo_baseurl"><?php _e('Base URL path of matomo installation', 'simple-matomo-tracking-code'); ?></label>
 						</th>
 						<td>
 							<input id="matomo_baseurl" name="matomo_baseurl" type="text" size="40" maxlength="99" value="<?php echo $options['matomo_baseurl']; ?>" /><br/>
 							<div id="expl2" style="display:none;">
-								<p>The URL path to the matomo installation. E.g. /matomo/ or /stats/. Don't forget the trailing slash!</p>
+								<p>
+								<?php _e("The URL path to the matomo installation. E.g. /matomo/ or /stats/. Don't forget the trailing slash!", 'simple-matomo-tracking-code'); ?>
+								</p>
 							</div>
 						</td>
 					</tr>
 					<tr>
 						<th scope="row" valign="top">
-							<label for="admintracking">Track the admin user too</label><br/>
-							<small>(default is not to)</small>
+							<label for="admintracking"><?php _e('Track the admin user too', 'simple-matomo-tracking-code'); ?></label><br/>
+							<small><?php _e('(default is not to)', 'simple-matomo-tracking-code'); ?></small>
 						</th>
 						<td>
 							<input type="checkbox" id="admintracking" name="admintracking" <?php if ($options['admintracking']) echo ' checked="checked" '; ?>/> 
 						</td>
 					</tr>
 					</table>
-					<p style="border:0;" class="submit"><input type="submit" name="submit" value="Update Settings &raquo;" /></p>
+					<p style="border:0;" class="submit"><input type="submit" name="submit" value="<?php echo esc_html__('Save settings', 'simple-matomo-tracking-code'); ?>" /></p>
 				</form>
-				<p>All options set? Then <a href="http://<?php if ($options['matomo_host']) { echo $options['matomo_host']; }else{ echo $_SERVER['HTTP_HOST'];}; echo $options['matomo_baseurl']; ?>" title="Matomo admin url" target="_blank">check out your stats!</a>
+				<p>
+				<?php
+				if ( $options['matomo_host'] ) {
+					$matomo_url = "//" . $options['matomo_host'];
+					$matomo_url = rtrim($matomo_url, '/') . '/';
+					$matomo_url = $matomo_url . $options['matomo_baseurl'];
+					$matomo_url = rtrim($matomo_url, '/') . '/';
+				} else {
+					$matomo_url = $options['matomo_baseurl'];
+					$matomo_url = rtrim($matomo_url, '/') . '/';
+				}
+				printf(
+					/* translators: %s: URL of the Matomo installation */
+					__('All options set? Then <a href="%s" title="Matomo admin url" target="_blank">check out your stats!</a>', 'simple-matomo-tracking-code'),
+					$matomo_url
+				);
+				?>
 			</div>
 			<?php
 			if (isset($options['siteid'])) {
@@ -176,29 +201,28 @@ if ( ! class_exists( 'SMTC_Admin' ) ) {
 					add_action('admin_footer', array('SMTC_Admin','warning'));
 				} else {
 					if (isset($_POST['submit'])) {
-						if ($_POST['siteid'] != $options['siteid'] ) {
-							add_action('admin_footer', array('SMTC_Admin','success'));
-						}
+						add_action('admin_footer', array('SMTC_Admin','success'));
 					}
 				}
 			} else {
 				add_action('admin_footer', array('SMTC_Admin','warning'));
 			}
-
-		} // end config_page()
+		}
 
 		function success() {
 			echo "
-			<div id='analytics-warning' class='updated fade-ff0000'><p><strong>Congratulations! You have just activated Matomo Web Analytics.</p></div>
-			<style type='text/css'>
-			#adminmenu { margin-bottom: 7em; }
-			#analytics-warning { position: absolute; top: 7em; }
-			</style>";
+			<div id='analytics-warning' class='updated'><p><strong>";
+			_e('Simple Matomo Tracking Code Configuration successfully updated.', 'simple-matomo-tracking-code');
+			echo "</strong></p></div>";
 		}
 
 		function warning() {
 			echo "
-			<div id='analytics-warning' class='updated fade-ff0000'><p><strong>Matomo Web Analytics is not active.</strong> You must <a href='plugins.php?page=simple-matomo-tracking-code.php'>enter your Site ID</a> for it to work.</p></div>";
+			<div id='analytics-warning' class='notice notice-warning'><p><strong>";
+			_e('Matomo Web Analytics is not active.', 'simple-matomo-tracking-code');
+			echo "</strong> ";
+			_e('You must enter your Site ID for it to work.', 'simple-matomo-tracking-code');
+			echo "</p></div>";
 		}
 	}
 }
